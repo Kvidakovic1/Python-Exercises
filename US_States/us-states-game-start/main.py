14740a99a25d30c0a2972368215c3361c4f11290
Kristijan Vidaković <kiki11223333@gmail.com>
2022-01-09 02:08:57 +0100

    Guess_The_State

---
+++ US_States/us-states-game-start/main.py
@@ -0,0 +1,62 @@
+import turtle
+import pandas
+
+
+
+
+
+
+screen = turtle.Screen()
+screen.title("U.S. States  Game")
+image = "blank_states_img.gif"
+screen.addshape(image)
+turtle.shape(image)
+
+data = pandas.read_csv("50_states.csv")
+all_states = data["state"].tolist()
+print(all_states)
+
+guessed_states = []
+
+
+while len(guessed_states) < 50:
+    answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct", prompt="What's another states's name?").title()
+
+    if answer_state == "Exit":
+        not_guessed = []
+        for state in all_states:
+            if state not in guessed_states:
+                not_guessed.append(state)
+        new_data = pandas.DataFrame(not_guessed)
+        new_data.to_csv("states_to_learn.csv")
+        break
+    if answer_state in all_states:
+        guessed_states.append(answer_state)
+        all_states.remove(answer_state)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data.state == answer_state]
+        t.goto(int(state_data.x), int(state_data.y))
+        t.write(answer_state)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
