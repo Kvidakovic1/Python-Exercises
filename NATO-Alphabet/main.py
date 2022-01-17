656cc0a7298089bf4cc793179d96a84ea39f049a
Kristijan Vidaković <kiki11223333@gmail.com>
2022-01-17 23:41:57 +0100

    Catching exceptions
    Implemented checking users input for mistake and catching it

--- NATO-Alphabet/main.py
+++ NATO-Alphabet/main.py
@@ -3,10 +3,17 @@ import pandas
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
 
 
-bet = {row.letter:row.code for (index, row) in data.iterrows()}
+dict = {row.letter:row.code for (index, row) in data.iterrows()}
+print(dict)
 
+def generate_phonetic():
+    word = input("Enter a word:").upper()
+    try:
+        output_list = [dict[letter] for letter in word]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+        generate_phonetic()
+    else:
+        print(output_list)
 
-
-word = input("Enter a word:").upper()
-output_list = [bet[letter] for letter in word]
-print(output_list)
+generate_phonetic()
